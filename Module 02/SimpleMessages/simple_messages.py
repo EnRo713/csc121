9c03a52686b4b22f454f41eb92b0e5ae9c3ada25
EnRo713 <enriquearodriguez@students.abtech.edu>
2024-01-28 21:50:45 -0500

    Initial commit

---
+++ Module 02/SimpleMessages/simple_messages.py
@@ -0,0 +1,7 @@
+# Assigning a message to a variable and printing it
+message = "Hello, world!"
+print(message)
+
+# Changing the value of the variable and printing the new message
+message = "This is a new message."
+print(message)
