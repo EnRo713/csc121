e5f5f616c4c211de29b0bb74eb7846f1a1be41fa
EnRo713 <enriquearodriguez@students.abtech.edu>
2024-01-28 22:13:17 -0500

    Add files

---
+++ Module 02/FamousQuote/famous_quote.py
@@ -0,0 +1,6 @@
+# Define the quote and its author
+quote = "If you want to find the secrets of the universe, think in terms of energy, frequency, and vibration."
+author = "Nikola Tesla"
+
+# Print the quote with the author's name
+print(author + " once said, \"" + quote + "\"")
