e5785e18a82bc44b3d86169b5673604c17cb3962
EnRo713 <enriquearodriguez@students.abtech.edu>
2024-01-28 22:17:02 -0500

    Add files

---
+++ Module 02/NumberEight/number_eight.py
@@ -0,0 +1,15 @@
+# Addition
+print("Addition:")
+print(5 + 3)
+
+# Subtraction
+print("\nSubtraction:")
+print(10 - 2)
+
+# Multiplication
+print("\nMultiplication:")
+print(4 * 2)
+
+# Division
+print("\nDivision:")
+print(16 / 2)
