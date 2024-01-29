2a64a0a1577d368ca7fb2eb1df156ff4d5d88c18
EnRo713 <enriquearodriguez@students.abtech.edu>
2024-01-28 22:07:49 -0500

    Add files

---
+++ Module 02/NameCases/name_cases.py
@@ -0,0 +1,11 @@
+# Assigning a person's name to a variable
+person_name = "john doe"
+
+# Printing the name in lowercase
+print("Lowercase:", person_name.lower())
+
+# Printing the name in uppercase
+print("Uppercase:", person_name.upper())
+
+# Printing the name in title case
+print("Title Case:", person_name.title())
