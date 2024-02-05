6847871b93322415f87e8be475d85bc2ec3c3dcd
EnRo713 <enriquearodriguez@students.abtech.edu>
2024-02-04 20:54:15 -0500

    Add Module 3 files

---
+++ Module 03/3-4. GuestList/my_guest_list.py
@@ -0,0 +1,14 @@
+# Define a list of people to invite to dinner
+guest_list = [
+    "Salvador Dali",
+    "Arnold Schwarzenegger",
+    "Selena Quintanilla",
+    "Bruce Lee",
+    "Alan Watts",
+    "Michelle Yeoh",
+    "Jimi Hendrix"
+]
+
+# Use the list to print an invitation message to each person
+for guest in guest_list:
+    print("Dear", guest + ",\nI would like to invite you to dinner. Please join me for an evening of good food and conversation.\nSincerely,\nRich Rodriguez")
