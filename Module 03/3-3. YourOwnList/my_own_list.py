0c530b57aae5208ac0a46db3643de81fb06a413f
EnRo713 <enriquearodriguez@students.abtech.edu>
2024-02-04 20:40:29 -0500

    Add Module 3 files

---
+++ Module 03/3-3. YourOwnList/my_own_list.py
@@ -0,0 +1,13 @@
+# Define a list to store examples of transportation
+transportation_list = [
+    "Lexus LC",
+    "Honda CB750 motorcycle",
+    "1973 Ford Falcon XB GT Hardtop",
+    "ATV",
+    "Toyota Tacoma",
+    "1987 Buick GNX"
+]
+
+# Use the list to print statements about each item
+for item in transportation_list:
+    print("I would like to own a", item + ".")
