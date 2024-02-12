f7d214c487240310f17bae89d3211f9c6cd11b1e
EnRo713 <enriquearodriguez@students.abtech.edu>
2024-02-11 19:13:36 -0500

    Add Module 4 files

---
+++ Module 04/4-2. Animals/animals.py
@@ -0,0 +1,14 @@
+# Define a list of animals with a unique common characteristic
+animals = ["Platypus", "Echidna", "Spiny anteater"]
+
+# Print the name of each animal using a for loop
+print("Animals:")
+for animal in animals:
+    print(animal)
+
+# Print a statement about each animal
+for animal in animals:
+    print("The", animal.lower(), "is a monotreme.")
+
+# Print a statement about what these animals have in common
+print("All of these animals lay eggs and produce milk.")
